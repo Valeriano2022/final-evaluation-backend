195925930589ef0e8e0928a3e73907963490e716
Valey Austine Senoy <valeyaustinesenoy@gmail.com>
2025-12-10 09:17:32 +0800

    feat: implement jpa auditing and auditor aware

---
+++ src/main/kotlin/com/example/final_evaluation_backend/config/AuditorAware.kt
@@ -0,0 +1,22 @@
+package com.example.final_evaluation_backend.config
+
+import org.springframework.data.domain.AuditorAware
+import org.springframework.security.authentication.AnonymousAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+import java.util.Optional
+
+@Component("auditorAware")
+class AuditorAware : AuditorAware<Long> {
+    override fun getCurrentAuditor(): Optional<Long> {
+        val auth = SecurityContextHolder.getContext().authentication
+        return when {
+            auth == null || !auth.isAuthenticated || auth is AnonymousAuthenticationToken ->
+                Optional.of(0)
+            auth.principal is CustomUserPrincipal ->
+                Optional.of((auth.principal as CustomUserPrincipal).userId)
+            else ->
+                Optional.ofNullable((auth.principal as CustomUserPrincipal).userId)
+        }
+    }
+}
---
+++ src/main/kotlin/com/example/final_evaluation_backend/config/JpaAuditingConfig.kt
@@ -0,0 +1,6 @@
+package com.example.final_evaluation_backend.config
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class JpaAuditingConfig
