0b22046a6195c730cf22d559d997e59db8b7d953
Valey Austine Senoy <valeyaustinesenoy@gmail.com>
2025-12-10 08:41:07 +0800

    initial commit

---
+++ src/main/kotlin/com/example/final_evaluation_backend/FinalEvaluationBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.final_evaluation_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinalEvaluationBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinalEvaluationBackendApplication>(*args)
+}
